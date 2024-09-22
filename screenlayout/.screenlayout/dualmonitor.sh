1eef10b425da031276ed77fecc1c06c1ae15a80d
Gopal-Kshetri <075bct039.gopal@pcampus.edu.np>
2024-09-22 16:08:42 +0545

    fix: polybar and scrrenlayout test changes

--- screenlayout/.screenlayout/dualmonitor.sh
+++ screenlayout/.screenlayout/dualmonitor.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off --output DP-6 --off --output DP-7 --off --output DP-8 --off --output HDMI-1-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output eDP-1 --mode 1920x1080 --rotate normal --output HDMI-1-0 --primary --mode 1920x1080 --rotate normal --right-of eDP-1
+# xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal --output HDMI-1-0 --off
+# xrandr --output eDP-1 --mode 1920x1080 --rotate normal --output HDMI-1-0 --primary --mode 1920x1080 --rotate normal --right-of eDP-1
