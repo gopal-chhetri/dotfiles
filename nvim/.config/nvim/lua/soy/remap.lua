51b23dc3e7c08412a8099b4eaa954e41a387e108
Gopal-Kshetri <075bct039.gopal@pcampus.edu.np>
2024-10-25 16:33:04 +0545

    feat: move selected up and down nvim remap

--- nvim/.config/nvim/lua/soy/remap.lua
+++ nvim/.config/nvim/lua/soy/remap.lua
@@ -5,6 +5,14 @@ vim.keymap.set('n', '<leader>h', vim.cmd.Ex)
 vim.keymap.set('n', '<C-S-Up>', '<cmd>m -2<CR>')
 vim.keymap.set('n', '<C-S-Down>', '<cmd>m +1<CR>')
 
+-- Move highlighted lines up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Move up and down without moving the cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 -- Yank from vim and save it to clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
